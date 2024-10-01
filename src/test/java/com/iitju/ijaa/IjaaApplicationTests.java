f6813ced9aefd235a3f91a6a4c58e65f34ce9fc8
sahal-bs23 <sahal@brainstation-23.com>
2024-10-01 12:58:48 +0600

    initialized project

---
+++ src/test/java/com/iitju/ijaa/IjaaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iitju.ijaa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IjaaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
