a7eb2f437d9bf0e986bd596e57bb3c57d8ca6fe1
Sahal2000 <me.sahal2000@gmail.com>
2024-10-08 16:37:39 +0600

    cors config, user status update

--- src/main/java/com/iitju/ijaa/config/SecurityConfig.java
+++ src/main/java/com/iitju/ijaa/config/SecurityConfig.java
@@ -33,7 +33,7 @@ public class SecurityConfig {
         return http
                 .csrf(customizer -> customizer.disable())
                 .authorizeHttpRequests(request -> request
-                        .requestMatchers("/register","/login")
+                        .requestMatchers("/register","/login","/users")
                         .permitAll()
                         .anyRequest().authenticated())
                 .httpBasic(Customizer.withDefaults())
