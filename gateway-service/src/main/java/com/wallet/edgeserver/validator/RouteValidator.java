fdfa632140c8060e5b113110843f9f74cdfb321f
sahal-bs23 <sahal@brainstation-23.com>
2025-07-21 16:30:30 +0600

    Sign in, Sign up

---
+++ gateway-service/src/main/java/com/wallet/edgeserver/validator/RouteValidator.java
@@ -0,0 +1,15 @@
+package com.wallet.edgeserver.validator;
+
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+
+import java.util.function.Predicate;
+import java.util.stream.Stream;
+
+@Component
+public class RouteValidator {
+
+    public Predicate<ServerHttpRequest> isSecured = request ->
+            Stream.of("/ijaa/api/v1/user/signup","/ijaa/api/v1/user/signin")
+                    .noneMatch(uri -> request.getURI().getPath().contains(uri));
+}
